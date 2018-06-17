1355fa218c658e10a311e7d5400000284ff90b3f
YosefAhmed <yos7efahmed@gmail.com>
2018-06-17 17:08:47 +0200

    first test

---
+++ Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_demo
+{
+    class Employee
+    {
+        //properties
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public decimal Salary { get; set; }
+        //methods
+        public String Report()
+        {
+            return "ID :" + ID + "\nName :" + Name + "\nCity :" + City + "\nSalary :" + Salary;
+        }
+        //events
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oop_demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //classname opjectname = new classname();
+            Employee emp = new Employee();
+            emp.ID = 1;
+            emp.Name = "Yousef";
+            emp.City = "Cairo";
+            emp.Salary = 1200;
+            var rprt=emp.Report();
+            MessageBox.Show(rprt); 
+        }
+    }
+}
